43de84c8a3943dddf01f2d4c237081d5719c9f43
kumuthiniak <kumuthini.program@gmail.com>
2025-09-06 14:12:47 +0530

    item class created

---
+++ NCC_2025/backend2/src/main/java/com/example/demo/entity/Item.java
@@ -0,0 +1,19 @@
+package com.example.demo.entity;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+
+
+public class Item {
+
+
+    private int itemCode;
+    private int unitPrice;
+    private LocalDateTime updatedDate;
+
+    
+
+
+   
+    
+}
