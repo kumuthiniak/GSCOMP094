758b3b7c7c637761d6d232e6334f2338d84b1480
kumuthiniak <kumuthini.program@gmail.com>
2025-09-06 14:18:45 +0530

    getter and setters created

--- NCC_2025/backend2/src/main/java/com/example/demo/entity/Item.java
+++ NCC_2025/backend2/src/main/java/com/example/demo/entity/Item.java
@@ -1,19 +1,46 @@
 package com.example.demo.entity;
-
-import java.sql.Date;
-import java.time.LocalDateTime;
-
+import java.time.LocalDate;
 
 public class Item {
 
 
-    private int itemCode;
-    private int unitPrice;
-    private LocalDateTime updatedDate;
+    private String itemCode;
 
-    
+    private Double unitPrice;
+
+    private LocalDate updatedDate;
+
+    // Constructors
+    public Item() {}
+
+    public Item(String itemCode, Double unitPrice, LocalDate updatedDate) {
+        this.itemCode = itemCode;
+        this.unitPrice = unitPrice;
+        this.updatedDate = updatedDate;
+    }
 
 
-   
-    
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    public Double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(Double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public LocalDate getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(LocalDate updatedDate) {
+        this.updatedDate = updatedDate;
+    }
 }
