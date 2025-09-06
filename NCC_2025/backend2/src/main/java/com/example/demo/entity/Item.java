1e375c442fa0fcbce4efce1a1ac5e75c7fd177c1
kumuthiniak <kumuthini.program@gmail.com>
2025-09-06 15:05:32 +0530

    auto increment id added

--- NCC_2025/backend2/src/main/java/com/example/demo/entity/Item.java
+++ NCC_2025/backend2/src/main/java/com/example/demo/entity/Item.java
@@ -2,12 +2,19 @@ package com.example.demo.entity;
 import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
 @Data
 public class Item {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;  // <-- primary key
 
     private String itemCode;
 
@@ -60,3 +67,5 @@ public class Item {
         this.updatedDate = updatedDate;
     }
 }
+
+
